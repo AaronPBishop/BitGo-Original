69a8fc9ddf60e5d38a8408c26cc45f67f2130712
AaronPBishop <aaronpbishop@gmail.com>
2022-08-18 22:15:22 -0500

    Built win checking functions

--- public/Scripts/board-4x4.js
+++ public/Scripts/board-4x4.js
@@ -334,28 +334,81 @@ export default class Board4x4 {
         for (let row = 0; row < board.length; row++) {
             let currRow = `r${row}`;
             for (let col = 0; col < board[row].length; col++) {
-
                 rows[currRow].push(this.grid[row][col]);
             };
         };
 
         const rowValues = Object.values(rows);
+        const visited = new Set();
         for (let row = 0; row < rowValues.length; row++) {
-            let currRow = rowValues[row].toString();
-            for (let next = 0; next < rowValues.length; next++) {
-                let nextRow = rowValues[next].toString();
+                let currRow = Number(rowValues[row].join(''));
 
-                if (currRow === nextRow) return false;
-            };
+                if (visited.has(currRow)) return false;
+                visited.add(currRow);
         };
 
         return true;
     };
 
+    checkCols(board) {
+        const cols = {
+            c0: [],
+            c1: [],
+            c2: [],
+            c3: []
+        };
+
+        for (let row = 0; row < board.length; row++) {
+            for (let col = 0; col < board[row].length; col++) {
+                let currCol = `c${col}`;
+
+                cols[currCol].push(this.grid[row][col]);
+            };
+        };
+
+        const colValues = Object.values(cols);
+        const visited = new Set();
+        for (let col = 0; col < colValues.length; col++) {
+                let currCol = Number(colValues[col].join(''));
+
+                if (visited.has(currCol)) return false;
+                visited.add(currCol);
+        };
+
+        return true;
+    };
+
+    checkTotal(board) {
+        const values = {
+            r0: 0,
+            r1: 0,
+            r2: 0,
+            r3: 0
+        };
+
+        for (let row = 0; row < board.length; row++) {
+            const currRow = `r${row}`
+            for (let col = 0; col < board[row].length; col++) {
+                let currColVal = board[row][col];
+
+                values[currRow] += currColVal;
+            };
+        };
+
+        let check = 0;
+        Object.values(values).forEach(val => {if (Number(val) === 2) check++});
+
+        if (check === 4) return true;
+        return false;
+    };
+
     checkWin(board) {
         if (this.isBoardFull(board)) {
-            console.log('Board is full');
-            console.log(this.checkRows(board))
+            if (this.checkRows(board)) {
+                if (this.checkCols(board)) {
+                    if (this.checkTotal(board)) return true;
+                };
+            };
         };
     };
 };
